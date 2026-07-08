793940379c534266f04c83885b62402132657714
Евгений Уразов <uragan@Mac.my>
2026-07-08 15:26:01 +0500

    fix: add nice headers to mega_hosts_template

--- generate.sh
+++ generate.sh
@@ -13,28 +13,52 @@ touch lists/banned_ips.txt
 # 1. Качаем файл GeoHideDNS
 curl -sL https://raw.githubusercontent.com/Internet-Helper/GeoHideDNS/main/hosts/hosts > geohide.tmp
 
-# 2. Вырезаем только нужные сервисы с помощью умного awk
+# 2. Вырезаем только нужные сервисы с заголовками и без дубликатов
 awk -v wanted="$SERVICES_TO_PROXY" '
 BEGIN {
     split(wanted, w, ",");
     for (i in w) { targets["# " w[i]] = 1 }
 }
-/^# / { extract = ($0 in targets) ? 1 : 0 }
-extract && /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ && $1 != "0.0.0.0" { print $2 }
-' geohide.tmp | sort -u > filtered_services.tmp
+/^# / { 
+    if ($0 in targets) { 
+        extract = 1
+        print "\n" $0 
+    } else { 
+        extract = 0 
+    }
+}
+extract && /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ && $1 != "0.0.0.0" { 
+    domain = $2
+    if (!seen[domain]) {
+        seen[domain] = 1
+        print domain
+    }
+}
+' geohide.tmp > filtered_services_with_headers.tmp
 
-# 3. Сохраняем эти домены для Nginx (в custom.txt)
-cat filtered_services.tmp > lists/custom.txt
+# 3. Сохраняем эти домены для Nginx (в custom.txt), убирая пустые строки и комментарии
+awk '!/^#/ && !/^$/ {print $0}' filtered_services_with_headers.tmp > lists/custom.txt
 
 # 4. Качаем файл от dns.malw.link чисто ради блокировки рекламы (0.0.0.0)
 curl -sL https://raw.githubusercontent.com/ImMALWARE/dns.malw.link/master/hosts > malw.tmp
 
 # 5. Формируем шаблон для роутера
-# Сначала забираем все 0.0.0.0 (рекламу) из malw
-awk '/^0\.0\.0\.0/ {print $0}' malw.tmp > mega_hosts_template.txt
+echo "# ==============================" > mega_hosts_template.txt
+echo "# БЛОКИРОВКА РЕКЛАМЫ (dns.malw.link)" >> mega_hosts_template.txt
+echo "# ==============================" >> mega_hosts_template.txt
+awk '/^0\.0\.0\.0/ {print $0}' malw.tmp >> mega_hosts_template.txt
 
-# Затем добавляем наши выбранные сервисы с IP-адресом VPS
-awk '{print "YOUR_VPS_IP " $1}' filtered_services.tmp >> mega_hosts_template.txt
+echo "" >> mega_hosts_template.txt
+echo "# ==============================" >> mega_hosts_template.txt
+echo "# ПРОКСИРОВАНИЕ СЕРВИСОВ (GeoHide)" >> mega_hosts_template.txt
+echo "# ==============================" >> mega_hosts_template.txt
+
+# Добавляем выбранные сервисы, сохраняя заголовки блоков
+awk '{
+  if (/^#/ || /^$/) { print $0 }
+  else { print "YOUR_VPS_IP " $1 }
+}' filtered_services_with_headers.tmp >> mega_hosts_template.txt
+
 
 # 6. Добавляем системные домены для работы роутера и GitHub
 for gh_domain in "raw.githubusercontent.com" "release-assets.githubusercontent.com" "private-user-images.githubusercontent.com" "gist.githubusercontent.com" "avatars.githubusercontent.com"; do
