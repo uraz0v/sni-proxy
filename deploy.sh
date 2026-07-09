6ba519ba5e2d9889d16cf546ae390c48e9f6959b
Евгений Уразов <uragan@Mac.my>
2026-07-09 13:57:14 +0500

    feat: automatically set Yekaterinburg timezone on deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "Setting system timezone to Asia/Yekaterinburg..."
+timedatectl set-timezone Asia/Yekaterinburg || echo "Failed to set timezone. Ignoring."
+
 echo "Installing Docker if not present..."
 docker compose version || (curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh)
 
